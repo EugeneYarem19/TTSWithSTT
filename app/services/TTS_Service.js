fd2c4422c98182a9fd429148d677f58e591938c5
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-02-27 17:01:39 +0200

    tts TTS updating

--- app/services/TTS_Service.js
+++ app/services/TTS_Service.js
@@ -2,16 +2,22 @@ import TTS from 'react-native-tts';
 
 export const TTS_Service = {
   initTTS: (callback = () => {}) =>
-    TTS.getInitStatus().then(() => {
-      TTS.setDefaultLanguage('en-IE');
-      TTS.setDefaultVoice('com.apple.ttsbundle.Moira-compact');
-      TTS.setDefaultPitch(1.5);
-      TTS.setDucking(true);
-      TTS.voices().then(voices => console.log(voices));
-      TTS.setIgnoreSilentSwitch('ignore');
+    TTS.getInitStatus()
+      .then(() => {
+        TTS.setDefaultLanguage('en-IE');
+        TTS.setDefaultVoice('com.apple.ttsbundle.Moira-compact');
+        TTS.setDefaultPitch(1.5);
+        TTS.setDucking(true);
+        TTS.voices().then(voices => console.log(voices));
+        TTS.setIgnoreSilentSwitch('ignore');
 
-      callback();
-    }),
+        callback();
+      })
+      .catch(err => {
+        if (err.code === 'no_engine') {
+          TTS.requestInstallEngine();
+        }
+      }),
 
   say: text =>
     TTS.speak(text, {
